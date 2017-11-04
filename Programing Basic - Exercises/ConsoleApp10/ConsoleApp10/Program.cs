81996f66c4a7d1f9810539702918619600a753f7
Your Name <ivvo98@abv.bg>
2017-11-05 00:45:29 +0200

    Basic

---
+++ Programing Basic - Exercises/ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            {
+                var n = int.Parse(Console.ReadLine());
+                var arr = new List<int>();
+                for (var i = 0; i < n; i++)
+                {
+                    arr.Add(int.Parse(Console.ReadLine()));
+                }
+
+                foreach (var item in arr)
+                {
+                    if (item == arr.Sum() - item)
+                    {
+                        var sum = arr.Sum() - item;
+                        Console.WriteLine("Yes");
+                        Console.WriteLine("Sum  = " + sum);
+                        return;
+                    }
+                }
+                Console.WriteLine("No");
+                var max = arr.Max();
+                arr.Remove(arr.Max());
+                Console.WriteLine("Diff = " + Math.Abs(arr.Sum() - max));
+            }
+        }
+    }
+}
---
+++ Programing Basic - Exercises/ConsoleApp10/Problem 1 – Wormtest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1___Wormtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal lenght = decimal.Parse(Console.ReadLine()) * 100;
+            decimal width = decimal.Parse(Console.ReadLine());
+
+            if (width == 0)
+            {
+                Console.WriteLine($"0.00");
+                return;
+            }
+            if (lenght % width == 0)
+            {
+                var sum = lenght * width;
+                Console.WriteLine($"{sum:f2}");
+            }
+            else
+            {
+                var sum = (lenght / width) * 100;
+                Console.WriteLine($"{sum:f2}%");
+            }
+        }
+    }
+}

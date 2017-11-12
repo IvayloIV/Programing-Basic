786149271d6517134110de2dd05030f57cb41cad
Your Name <ivvo98@abv.bg>
2017-11-12 12:23:56 +0200

    Basic

---
+++ Programing Basic - Exercises/sasd/4. Намиране на по-голямото число/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Намиране_на_по_голямото_число
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new List<int>();
+            for (int i = 0; i < 2; i++)
+            {
+                arr.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine(arr.Max());
+        }
+    }
+}
---
+++ Programing Basic - Exercises/sasd/7. Сумиране на секунди/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Сумиране_на_секунди
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secounds = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                secounds += int.Parse(Console.ReadLine());
+            }
+            var currentSecounds = secounds % 60;
+            var currentMinutes = (secounds / 60) % 60;
+            Console.WriteLine($"{currentMinutes}:{currentSecounds:d2}");
+        }
+    }
+}

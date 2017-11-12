92d215d30908cdc12bf60c6fe25aef89a1b8ddb9
Your Name <ivvo98@abv.bg>
2017-11-12 12:18:44 +0200

    Basic

---
+++ Programing Basic - Exercises/1/13. 1000 Days After Birth/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysAfterBirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture)
+                .AddDays(999)
+                .ToString("dd-MM-yyyy"));
+        }
+    }
+}
---
+++ Programing Basic - Exercises/2/07. Left and Right Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var leftPart = 0;
+            for (int i = 0; i < n; i++)
+            {
+                leftPart += int.Parse(Console.ReadLine());
+            }
+
+            var rightPart = 0;
+            for (int i = 0; i < n; i++)
+            {
+                rightPart += int.Parse(Console.ReadLine());
+            }
+
+            if (leftPart == rightPart)
+            {
+                Console.WriteLine($"Yes, sum = {leftPart}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftPart - rightPart)}");
+            }
+        }
+    }
+}
